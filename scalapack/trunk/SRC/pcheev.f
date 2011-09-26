ab860b99db496655e16a28f4a21eacf40ec53211
julie <julie@0ab0ef91-a80a-0410-bd19-f1b1e7e485ae>
2011-09-26 21:17:24 +0000

    Fix description of W

--- scalapack/trunk/SRC/pcheev.f
+++ scalapack/trunk/SRC/pcheev.f
@@ -126,8 +126,7 @@
 *          correct error reporting.
 *
 *  W       (global output) REAL array, dimension (N)
-*          On normal exit, the first M entries contain the selected
-*          eigenvalues in ascending order.
+*          If INFO=0, the eigenvalues in ascending order.
 *
 *  Z       (local output) COMPLEX array,
 *          global dimension (N, N),
