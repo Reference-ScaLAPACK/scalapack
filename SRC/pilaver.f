fb523022f72958e8994a13cf117d8cc849c7ea41
james <james@0ab0ef91-a80a-0410-bd19-f1b1e7e485ae>
2011-11-08 12:27:10 +0000

    added documentation

--- SRC/pilaver.f
+++ SRC/pilaver.f
@@ -5,6 +5,19 @@ C  -- ScaLAPACK is a software package provided by Univ. of Tennessee,    --
 C  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..--
 C     November 2011
 C
+C  Purpose
+C  =======
+C
+C  This subroutine return the ScaLAPACK version.
+C
+C  Arguments
+C  =========
+C  VERS_MAJOR   (output) INTEGER
+C      return the scalapack major version
+C  VERS_MINOR   (output) INTEGER
+C      return the scalapack minor version from the major version
+C  VERS_PATCH   (output) INTEGER
+C      return the scalapack patch version from the minor version
 C  =====================================================================
 C
       INTEGER VERS_MAJOR, VERS_MINOR, VERS_PATCH
