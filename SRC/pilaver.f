38f6b35b050cca7a4a74b45cd18ca29773462480
julie <16792376+julielangou@users.noreply.github.com>
2012-01-17 23:35:02 +0000

    Update version number

--- SRC/pilaver.f
+++ SRC/pilaver.f
@@ -1,9 +1,9 @@
       SUBROUTINE PILAVER( VERS_MAJOR, VERS_MINOR, VERS_PATCH )
 C
-C  -- ScaLAPACK computational routine (version 2.0 ) --
+C  -- ScaLAPACK computational routine (version 2.0.1 ) --
 C  -- ScaLAPACK is a software package provided by Univ. of Tennessee,    --
 C  -- Univ. of California Berkeley, Univ. of Colorado Denver and NAG Ltd..--
-C     November 2011
+C     January 2012
 C
 C  Purpose
 C  =======
@@ -24,7 +24,7 @@ C
 C  =====================================================================
       VERS_MAJOR = 2
       VERS_MINOR = 0
-      VERS_PATCH = 0
+      VERS_PATCH = 1
 C  =====================================================================
 C
       RETURN
