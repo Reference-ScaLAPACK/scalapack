3d190a83fb2b7e803092f289ca60b97ebfafd073
james <james@0ab0ef91-a80a-0410-bd19-f1b1e7e485ae>
2012-04-24 18:19:32 +0000

    fixed FORTRAN name hack

--- scalapack/trunk/SRC/clamov.c
+++ scalapack/trunk/SRC/clamov.c
@@ -6,14 +6,6 @@
 
 #define TYPE  complex
 #define FUNC  "CLAMOV"
-#if ( defined Add_ )
 #define LAMOV clamov_
 #define LACPY clacpy_
-#elif ( defined UpCase )
-#define LAMOV CLAMOV
-#define LACPY CLACPY
-#elif ( defined NoChange )
-#define LAMOV clamov
-#define LACPY clacpy
-#endif
 #include "lamov.h"
