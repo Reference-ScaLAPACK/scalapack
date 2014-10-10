f92120085704fc517986ca9adff1d86ee4cf61e5
julie <julie@0ab0ef91-a80a-0410-bd19-f1b1e7e485ae>
2014-10-10 19:23:10 +0000

    added ictxt to pxerbla call, fixes bug reported by eugene Chereshnev on LAPACK Forum http://icl.cs.utk.edu/lapack-forum/viewtopic.php?f=13&p=11064
    (same issue than bug fix done in r192)

--- scalapack/trunk/SRC/psgebal.f
+++ scalapack/trunk/SRC/psgebal.f
@@ -222,7 +222,7 @@
          CALL CHK1MAT( N, 2, N, 2, 1, 1, DESCA, 4, INFO )
       END IF
       IF( INFO.NE.0 ) THEN
-         CALL PXERBLA( 'PSGEBAL', -INFO )
+         CALL PXERBLA( ICTXT, 'PSGEBAL', -INFO )
          RETURN
       END IF
 *
