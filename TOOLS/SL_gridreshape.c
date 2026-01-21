3543688e1068c256b13d8ff89ff6f3d403e00275
adam-sim-dev <76020022+adam-sim-dev@users.noreply.github.com>
2026-01-21 10:16:40 +0800

    Update SL_gridreshape.c

--- TOOLS/SL_gridreshape.c
+++ TOOLS/SL_gridreshape.c
@@ -7,12 +7,11 @@
 
 Int SL_Cgridreshape(Int ctxt, Int pstart, Int row_major_in, Int row_major_out, Int P, Int Q)
 {
-
    /** called subprograms **/
-   void Cblacs_gridinfo(Int context, Int* nprow, Int* npcol, Int* myrow, Int* mycol);
-   void Cblacs_abort(Int ConTxt, Int ErrNo);
-   void Cblacs_get(Int context, Int what, Int * val);
-   void Cblacs_gridmap(Int* context, Int* usermap, Int ldumap, Int nprow, Int npcol);
+   void Cblacs_gridinfo( Int context, Int* nprow, Int* npcol, Int* myrow, Int* mycol );
+   void Cblacs_abort( Int ConTxt, Int ErrNo );
+   void Cblacs_get( Int context, Int what, Int * val );
+   void Cblacs_gridmap( Int* context, Int* usermap, Int ldumap, Int nprow, Int npcol );
 
    Int Cblacs_pnum( Int context, Int prow, Int pcol );
    Int nctxt, P0, Q0, Np, i, *g;
